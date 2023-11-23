561b382de52dd5cc1a148b95c78bb36d021bf2ea
root <siyamthanda.ndlovu.gasa@gmail.com>
2023-11-23 15:44:14 +0200

    Removed buttons

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,5 +1,9 @@
 console.log("Welcome to Zeya Time :)");
 
+//Beginning of application functionality
+addEventListener()
+
+
 function startTimer() {
     var hours = document.getElementById("hours").value;
     var minutes = document.getElementById("minutes").value;
