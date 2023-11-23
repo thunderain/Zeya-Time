a6b1d61d89715052802c107a0153065469bf36fc
root <siyamthanda.ndlovu.gasa@gmail.com>
2023-11-23 15:34:22 +0200

    Added unstyled component

---
+++ src/js/timer.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Zeya Time :)");
+
+function startTimer() {
+    var hours = document.getElementById("hours").value;
+    var minutes = document.getElementById("minutes").value;
+    var seconds = document.getElementById("seconds").value;
+  
+    var totalSeconds = hours * 3600 + minutes * 60 + seconds;
+  
+    // Start the timer logic here...
+  }
