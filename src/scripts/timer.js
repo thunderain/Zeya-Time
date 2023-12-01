ad8d69618206498ba755b4614b5a6177cc07a24b
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2023-12-01 13:11:59 +0200

    Adding variable declarations

--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log("Welcome to Zeya Time :)");
+console.log("Welcome to Zeya Time ^_^");
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let totalEndSeconds = 0;
+//Applications functions
--- src/ts/timer.ts
+++ src/ts/timer.ts
@@ -1,1 +1,9 @@
-console.log("Welcome to Zeya Time :)");
+console.log("Welcome to Zeya Time ^_^");
+
+let hours : number = 0;
+let minutes : number = 0;
+let seconds : number = 0;
+let totalEndSeconds : number = 0;
+
+//Applications functions
+
