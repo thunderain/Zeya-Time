df2d3d1234ae89d94a308c2dfd51bf627cdebb51
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2023-12-01 13:25:15 +0200

    setVariable()

--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -5,3 +5,30 @@ let minutes = 0;
 let seconds = 0;
 let totalEndSeconds = 0;
 //Applications functions
+function setTimeVariable(type, val) {
+    if (!type || !val)
+        return;
+    switch (type) {
+        case "hours": {
+            hours = val;
+            console.log("Hours has been set : ", hours);
+            break;
+        }
+        case "minutes": {
+            minutes = val;
+            console.log("Minutes has been set : ", minutes);
+            break;
+        }
+        case "seconds": {
+            seconds = val;
+            console.log("Seconds has been set : ", seconds);
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+}
+function getEndTime(currTime) {
+    return currTime.toISOString();
+}
--- src/ts/timer.ts
+++ src/ts/timer.ts
@@ -6,4 +6,35 @@ let seconds : number = 0;
 let totalEndSeconds : number = 0;
 
 //Applications functions
+function setTimeVariable(type : string, val : number){
+
+    if (!type || !val)
+        return;
+
+    switch (type) {
+        case "hours": {
+            hours = val;
+            console.log("Hours has been set : " , hours);
+            break;
+        }
+        case "minutes": {
+            minutes = val;
+            console.log("Minutes has been set : ",minutes);
+            break;
+        }
+        case "seconds": {
+            seconds = val;
+            console.log("Seconds has been set : ",seconds);
+            break;
+        }
+        default: {
+            break;
+        }
+        }
+          
+}
+
+function getEndTime(currTime : Date) : string {
+    return currTime.toISOString();
+}
 
